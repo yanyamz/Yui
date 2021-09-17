e1b207e88b88b611a1c72fcd878bfe15dcb69b2d
yanyamz <hello.anthony9898@gmail.com>
2021-09-17 01:10:48 -0700

    fsalf

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="container">
-        <router-view></router-view>
-    </div>
+	<div class="container">
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
@@ -9,29 +9,29 @@ import { mapMutations } from 'vuex'
 import { projectAuth } from '@/firebase/config'
 
 export default {
-    async mounted() {
-        projectAuth.onAuthStateChanged((_user) => {
-            this.updateUser(_user)
-        })
-    },
-    async unmounted() {
-        projectAuth.onAuthStateChanged((_user) => {
-            this.updateUser(_user)
-        })
-    },
-    methods: {
-        ...mapMutations('auth', ['updateUser']),
-    },
+	async mounted() {
+		projectAuth.onAuthStateChanged((_user) => {
+			this.updateUser(_user)
+		})
+	},
+	async unmounted() {
+		projectAuth.onAuthStateChanged((_user) => {
+			this.updateUser(_user)
+		})
+	},
+	methods: {
+		...mapMutations('auth', ['updateUser']),
+	},
 }
 </script>
 
 <style>
 #app {
-    padding: 0 1rem;
+	padding: 0 1rem;
 }
 
 * {
-    scroll-behavior: smooth;
-    box-sizing: border-box;
+	scroll-behavior: smooth;
+	box-sizing: border-box;
 }
 </style>
